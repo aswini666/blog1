61f765aa0f0d6a33bc9d7ed71ef9aee5c9700b5a
aswini666 <aswini.evergreen@gmail.com>
2020-11-18 20:54:22 +0530

    Generated comment controller and user able to add comment for particular post

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,15 @@
+class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(params.require(:comment).permit(:content, :post_id))
+    @comment.save
+    redirect_to @post, notice: "Comment created successfully"
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to posts_path, notice: "Comment deleted successfully"
+  end
+end
